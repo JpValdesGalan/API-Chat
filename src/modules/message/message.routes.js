b73bc3aef363b0c64937412b680ed6a92381dc76
JpValdesGalan <57958885+JpValdesGalan@users.noreply.github.com>
2022-03-12 17:55:47 -0600

    Routes documented with Swagger

--- src/modules/message/message.routes.js
+++ src/modules/message/message.routes.js
@@ -1,8 +1,66 @@
 const router = require('express').Router();
 const controller = require('./message.controller');
 
-router.get('/', controller.getAll);
+/**
+ * @swagger
+ *   /api/messages:
+ *     get:
+ *       tags:
+ *       - Messages
+ *       description: Get all messages
+ *       responses:
+ *         200:
+ *           description: Array with a list of 
+ */
+ router.get('/', controller.getAll);
 
-router.get('/:id', controller.getOne);
+ /**
+  * @swagger
+  *   /api/messages/{id}:
+  *     get:
+  *       tags:
+  *       - Messages
+  *       description: Get one message by ID
+  *       parameters:
+  *         - in: path
+  *           name: id
+  *           required: true
+  *           description: The message's unique ID
+  *       responses:
+  *         200:
+  *           description: An object with a single message's data
+  */
+ router.get('/:id', controller.getOne);
+
+/**
+ * @swagger
+ *   /api/messages:
+ *     post:
+ *       tags:
+ *       - Messages
+ *       description: Create a message
+ *       responses:
+ *         200:
+ *           description: Message created
+ */
+ router.post('/', controller.create);
+
+  /**
+  * @swagger
+  *   /api/messages/{id}:
+  *     post:
+  *       tags:
+  *       - Messages
+  *       description: Create a message
+  *       parameters:
+  *         - in: path
+  *           name: id
+  *           required: true
+  *           description: The message's unique ID
+  *       responses:
+  *         200:
+  *           description: Updated message
+  */
+   router.post('/:id', controller.update);
 
 module.exports = router;
--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,8 +1,66 @@
 const router = require('express').Router();
 const controller = require('./user.controller');
 
-router.get('/', controller.getAll);
+/**
+ * @swagger
+ *   /api/users:
+ *     get:
+ *       tags:
+ *       - Users
+ *       description: Get all users
+ *       responses:
+ *         200:
+ *           description: Array with a list of
+ */
+ router.get('/', controller.getAll);
 
-router.get('/:id', controller.getOne);
+ /**
+  * @swagger
+  *   /api/users/{id}:
+  *     get:
+  *       tags:
+  *       - Users
+  *       description: Get one user by ID
+  *       parameters:
+  *         - in: path
+  *           name: id
+  *           required: true
+  *           description: The user's unique ID
+  *       responses:
+  *         200:
+  *           description: An object with a single user's data
+  */
+ router.get('/:id', controller.getOne);
+ 
+ /**
+  * @swagger
+  *   /api/users:
+  *     post:
+  *       tags:
+  *       - Users
+  *       description: Create a user
+  *       responses:
+  *         200:
+  *           description: User created
+  */
+ router.post('/', controller.create);
+ 
+ /**
+  * @swagger
+  *   /api/users/{id}:
+  *     post:
+  *       tags:
+  *       - Users
+  *       description: Create a user
+  *       parameters:
+  *         - in: path
+  *           name: id
+  *           required: true
+  *           description: The user's unique ID
+  *       responses:
+  *         200:
+  *           description: Updated user
+  */
+ router.post('/:id', controller.update);
 
 module.exports = router;
